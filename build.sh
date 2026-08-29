8825d87fd52ce13597be265410d6a974c91f886d
osman gedik <osmangdk@gmail.com>
2026-08-29 16:57:57 +0300

    Switch to package.json for Vercel build

--- build.sh
+++
@@ -1,7 +0,0 @@
-﻿#!/bin/bash
-echo "Cloning Flutter..."
-git clone https://github.com/flutter/flutter.git -b stable
-echo "Adding Flutter to PATH..."
-export PATH="$PATH:$PWD/flutter/bin"
-echo "Building Flutter Web..."
-flutter build web --release
