3b38c5c840eea171233fd9f34a165daa0f6edee9
osman gedik <osmangdk@gmail.com>
2026-08-29 16:47:59 +0300

    Add Vercel build settings

---
+++ build.sh
@@ -0,0 +1,7 @@
+﻿#!/bin/bash
+echo "Cloning Flutter..."
+git clone https://github.com/flutter/flutter.git -b stable
+echo "Adding Flutter to PATH..."
+export PATH="$PATH:$PWD/flutter/bin"
+echo "Building Flutter Web..."
+flutter build web --release
