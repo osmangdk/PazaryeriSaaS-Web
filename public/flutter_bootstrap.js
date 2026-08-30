5c8e10a37c0d0a622017aad53d1324fa1db2f91e
osman gedik <osmangdk@gmail.com>
2026-08-30 23:26:12 +0300

    Mobile & Web: Deployed responsive Mobile UI updates + minSdk 28 Android Release APK

--- public/flutter_bootstrap.js
+++ public/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5d531788691ec3404cac0cee66ead4007b1773
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1607804367" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3748304619" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
